dcd0beb65a3429c885f6365b77bbccf7f304c8c3
MC3Mobile <mc3mobileapplications@gmail.com>
2018-02-28 15:51:04 -0500

    I push

--- app/src/main/java/com/mc3mobile/pulseoximetry/MainActivity.java
+++ app/src/main/java/com/mc3mobile/pulseoximetry/MainActivity.java
@@ -1,36 +1,102 @@
 package com.mc3mobile.pulseoximetry;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
-        import android.content.Intent;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.Button;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+//import com.github.mikephil.charting.utils.XLabels;
+//import com.github.mikephil.charting.utils.XLabels.XLabelPosition;
 
+import com.mc3mobile.pulseoximetry.R;
 
-        import com.mc3mobile.pulseoximetry.R;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    @Override
+    /* @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         Button b = (Button) findViewById(R.id.button);
-
-
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, Pop.class));
             }
 
-
         });
 
+    } */
 
-    }}
+    private LineChart mChart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // To make full screen layout
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                //WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        // Line Chart 1
+
+        mChart = (LineChart) findViewById(R.id.linechart); //defines linchart in xml
+
+        int[] data1 = {0,5,10,15,20,25,30,35,40,45}; // x and Y data values
+        int[] data2 = {97,98,96,98,97,96,97,98,99,97}; //see above
+
+        List<Entry> entries = new ArrayList<>(); // container for x,y entries
+
+        for (int i = 0; i < data1.length; i++) { //for loop, puts data in Entry
+
+            entries.add(new Entry(data1[i], data2[i])); //defines fill
+        }
+
+        LineDataSet dataSet = new LineDataSet(entries, "O2 Saturation"); // sets as a line, add entries to dataset
+        //dataSet.setColor(...);
+        //dataSet.setValueTextColor(...); // styling, ...
+
+        LineData lineData = new LineData(dataSet); //setting data
+        mChart.setData(lineData); //drawing the line
+        mChart.invalidate(); // refresh
+
+        XAxis xAxis1 = mChart.getXAxis();
+        xAxis1.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        // Line Chart 2
+
+        mChart = (LineChart) findViewById(R.id.linechart2); //defines linchart in xml
+
+        int[] data3 = {0,5,10,15,20,25,30,35,40,45}; // x and Y data values
+        int[] data4 = {61,62,63,61,63,62,61,63,63,61}; //see above
+
+       List<Entry> entries2 = new ArrayList<>(); // container for x,y entries
+
+        for (int i = 0; i < data3.length; i++) { //for loop, puts data in Entry
+
+            entries2.add(new Entry(data3[i], data4[i])); //defines fill
+        }
+
+        LineDataSet dataSet2 = new LineDataSet(entries2, "Pulse Rate"); // sets as a line, add entries to dataset
+        //dataSet.setColor();
+        //dataSet.setValueTextColor(...); // styling, ...
+
+        LineData lineData2 = new LineData(dataSet2); //setting data
+        mChart.setData(lineData2); //drawing the line
+        mChart.invalidate(); // refresh
+
+        XAxis xAxis2 = mChart.getXAxis();
+        xAxis2.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        }
+}
